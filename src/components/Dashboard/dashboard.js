149c2141d1883de4b7aa0eee2370fbe51109b6d6
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-10-17 12:07:06 -0600

    added dynamic background in dashboard

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -7,12 +7,37 @@ import NoteArea from '../NoteArea';
 import PlayerSearch from '../PlayerSearch'
 
 export class Dashboard extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            count: 0,
+            colors: ['#4D86EA', '#418DED', '#2A9AF4', '#1BA3F8', '#11A9FB'],
+            // currColor: this.state.colors[this.state.count]
+            currColor: ''
+        }
+
+    }
+
     componentDidMount(){   
         this.props.dispatch(fetchChampions())
+        this.interval = setInterval(() => {
+            this.setState({
+                currColor: this.state.colors[this.state.count],
+                count: this.state.count += 1
+            });
+            console.log(this.state.currColor, this.state.count);
+            // this.setState({count: this.state.count++});
+            if(this.state.count >= this.state.colors.length) this.setState({count: 0});
+        },2000);
     }
+
+    componentWillUnmount(){
+        clearInterval(this.interval);
+    }
+
     render(){
         return (
-            <main className='main-content'>
+            <main className='main-content' style={{background: this.state.currColor, transition: '2s'}}>
                 <section className='demo-players'>
                     <section>
                         <p>
@@ -49,4 +74,3 @@ export class Dashboard extends React.Component{
 }
 
 export default LoginWrapper()(connect()(Dashboard));
-
