e8b8c54a686ff59975d9b7a62750d6bce42fa415
AMJ <isaelwebdeveloper@gmail.com>
2019-02-13 17:51:21 -0700

    added styling to cards

--- src/components/TopCharts/TopCharts.js
+++ src/components/TopCharts/TopCharts.js
@@ -17,10 +17,26 @@ class TopCharts extends Component{
                 <div className='card-container'>
                     {/* this dive will be for the champions */}
                     <div className='left-side-card-container'>
-                        <TopChartsLeftCard styles={`left-cards sylas center-image`} text={'Test'}/>
-                        <TopChartsLeftCard styles={`left-cards lux center-image`} text={'Test'}/>
-                        <TopChartsLeftCard styles={`left-cards garen center-image`} text={'Test'}/>
-                        <TopChartsLeftCard styles={`left-cards garen center-image`} text={'Test'}/>
+                        <TopChartsLeftCard 
+                            styles={`left-cards sylas center-image`} 
+                            text={'Test'}
+                            side={'top-left'}    
+                        />
+                        <TopChartsLeftCard 
+                            styles={`left-cards lux center-image`} 
+                            text={'Test'}
+                            side={'top-right'}
+                            />
+                        <TopChartsLeftCard 
+                            styles={`left-cards garen center-image`} 
+                            text={'Test'}
+                            side={'bottom-left'}
+                            />
+                        <TopChartsLeftCard 
+                            styles={`left-cards garen center-image`} 
+                            text={'Test'}
+                            side={'bottom-right'}
+                            />
                     </div>
                     {/* this div will be for any recent changes to the site */}
                     <div className={`right-card center-image`}>
--- src/components/topChartsLeftCard/TopChartsLeftCard.js
+++ src/components/topChartsLeftCard/TopChartsLeftCard.js
@@ -2,21 +2,49 @@ import React, { Component } from 'react'
 import './styles.css'
 
 class TopChartsLeftCard extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            isCardHovered: false,
+            cardId: null,
+            hoverClass: null
+        }
+    }
+
+    animate(){
+        console.log(this.state.cardId)
+        this.setState({hoverClass: 'red'})
+    }
+
+    deanimate(){
+        this.setState({hoverClass: null})
+    }
+    
+    
     render(){
         console.log(this.props);
-
+        if(!this.state.cardId) this.setState({cardId: Math.random(100)})
         return (
-            <div className={this.props.styles}>
-                <div className={`card-info`}>
-                    <div className='test-border'>
-                      <div className='icon'>
-                        <img  src={require('../../images/championIcon.png')}/>
-                      </div>
+            // <div className={`${this.props.side}`}>    
+                <div 
+                    className={`${this.props.styles} ${this.state.hoverClass} linear ${this.props.side}`} 
+                    key={this.state.cardId} 
+                    onMouseEnter={() => this.animate()}
+                    onMouseLeave={() => this.deanimate()}
+                >
+                    <div className={`card-info`}>
+                        <div className='test-border'>
+                        <div className='icon'>
+                            <img  src={require('../../images/championIcon.png')}/>
+                        </div>
+                        </div>
+                        <p className={`title lol-theme`}>{this.props.text}</p>
+                        <p className={`description lol-theme2`}>dnsja dhskja dskjan asjkdn kjasdn sadasa njk njk</p>
+                        <button className={`hidden`}>Explore</button>
                     </div>
-                    <p className={`title lol-theme`}>{this.props.text}</p>
-                    <p className={`description lol-theme2`}>dnsja dhskja dskjan asjkdn kjasdn sadasa njk njk</p>
                 </div>
-            </div>
+            // </div>
         )
     }
 }
