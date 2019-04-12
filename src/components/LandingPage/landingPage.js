cd14c26c144c76b22db4bb403f241e36b5fc83c8
AJM <isaelwebdeveloper@gmail.com>
2019-04-12 12:11:21 -0600

    added transition animation for login form

--- src/components/LandingPage/landingPage.js
+++ src/components/LandingPage/landingPage.js
@@ -8,56 +8,76 @@ import Jumbotron from '../Jumbotron';
 import TopCharts from '../TopCharts';
 
 
-export function LandingPage(props){
-    if(props.loggedIn){
-        return <Redirect to='/dashboard'/>
+export class LandingPage extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = { 
+          moveLogin: false,
+          moveSignup: false
+        };
     }
-    
-    if(props.signedUp){
-        props.dispatch(signUpAgain());
-    }
+    render(){
+        const moveLogin = this.state.moveLogin;
+        const moveSignup = this.state.moveSignup;
 
-    return (
-        <main>
-            <Jumbotron test={
-                {
-                title: 'test',
-                background: null,
-                icon: null,
-                link: null,
-                text: 'lolololdksaoldjiosadjoasi ddjsiao das ijasdjklsd jasiokdjasi odkm asiodasmi odsmd oiasdm iasd smiodsa midoasd iasido ams'
+        if(this.props.loggedIn){
+            return <Redirect to='/dashboard'/>
+        }
+        
+        if(this.props.signedUp){
+            this.props.dispatch(signUpAgain());
+        }
+
+        return (
+            <main>
+                <Jumbotron test={
+                    {
+                    title: 'test',
+                    background: null,
+                    icon: null,
+                    link: null,
+                    text: 'lolololdksaoldjiosadjoasi ddjsiao das ijasdjklsd jasiokdjasi odkm asiodasmi odsmd oiasdm iasd smiodsa midoasd iasido ams'
+                    }
                 }
-            }
-            />
-            <TopCharts />
-            {/* <p className='landing-page-p'>
-            League of Legends is a fast-paced, competitive online game that blends the speed and intensity of an RTS with RPG elements. Two teams of powerful champions, each with a unique design and playstyle, battle head-to-head across multiple battlefields and game modes.
-                LoL Improver lets you take your gaming to a whole new level.
-            </p>
-            <p className='landing-page-p'>
-                DemoLogin: testuser
-            </p>
-            <p className='landing-page-p'>
-                DemoPassword: alongpassword
-            </p> */}
+                />
+                <TopCharts />
+                {/* <p className='landing-page-p'>
+                League of Legends is a fast-paced, competitive online game that blends the speed and intensity of an RTS with RPG elements. Two teams of powerful champions, each with a unique design and playstyle, battle head-to-head across multiple battlefields and game modes.
+                    LoL Improver lets you take your gaming to a whole new level.
+                </p>
+                <p className='landing-page-p'>
+                    DemoLogin: testuser
+                </p>
+                <p className='landing-page-p'>
+                    DemoPassword: alongpassword
+                </p> */}
 
-            <div className='landing-page-login-container'>
-                <div className='left-cover'>
-                    <h2>left cover</h2>
+                <div className='landing-page-login-container'>
+                    <div className='left-cover'>
+                        <h2>left cover</h2>
+                    </div>
+                    <div className={`${moveLogin ? 'moveSignup sign-up' : moveSignup ? 'moveSignupRight sign-up' : 'sign-up'} `}>
+                        <h2>sign up</h2>
+                        <button 
+                          onClick={() => this.setState({moveLogin: false, moveSignup: true})}>                          
+                          press</button>
+                    </div>
+                    <div 
+                      className={`${moveLogin ? 'moveLogin login' : moveSignup ? 'moveLoginRight login' :'login'}`} 
+                      // onAnimationEnd={() => this.setState({fade: false})}
+                      >
+                        <h2>login</h2>
+                        <button 
+                          ref='button'
+                          onClick={() => this.setState({moveLogin: true, moveSignup: false})}>
+                          press</button>
+                    </div>
                 </div>
-                <div className='login'>
-                    <h2>login</h2>
-                    <button onClick={() => console.log('hello')}>press</button>
-                </div>
-                <div className='sign-up'>
-                    <h2>sign up</h2>
-                    <button onClick={() => console.log('hello')}>press</button>
-                </div>
-            </div>
 
-            <LoginForm />
-        </main>
-    )
+                <LoginForm />
+            </main>
+        )
+    }
 }
 
 const mapStateToProps = state => {
