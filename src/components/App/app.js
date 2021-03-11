09fe94a94b81fad2435d324255a61162993b3b21
Isael Lizama <isaelwebdeveloper@gmail.com>
2021-03-11 13:50:01 -0700

    site is under construction

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {Route, withRouter} from 'react-router-dom';
+import { withRouter} from 'react-router-dom';
 import Dashboard from '../Dashboard';
 // import LandingPage from '../LandingPage';
 // import Header from '../Header';
@@ -14,7 +14,8 @@ export class App extends React.Component{
             <section className='main-body'>
                 {/* <Header /> */}
                 {/* <Route exact path='/' component={LandingPage} /> */}
-                <Route exact path='/' component={Dashboard} />
+                {/* <Route exact path='/' component={Dashboard} /> */}
+                <Dashboard />
                 {/* <Route exact path='/register' component={RegisterPage}/> */}
                 {/* <Footer /> */}
             </section>
