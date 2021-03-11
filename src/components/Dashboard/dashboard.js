be73873077f38a84c79fbfec856155a14f59f824
Isael Lizama <isaelwebdeveloper@gmail.com>
2021-03-10 20:21:12 -0700

    added more styling to left container

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -20,7 +20,7 @@ export class Dashboard extends React.Component{
                         <div className='left-container-width'>
                             <h1 className='left-container-title'>Level up your skill</h1>
                             <div className='left-container-buttons'>
-                                <div className='backdrop-wrapper-orange'>
+                                <div className='backdrop-wrapper-orange flex'>
                                     <button className='left-container-button'>Create Note</button>
                                 </div>
                                 <div className='three-dots-container'>
