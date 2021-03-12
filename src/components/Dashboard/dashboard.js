78be5db6938e8c722b9017dbff671a5da8fab3ef
Isael Lizama <isaelwebdeveloper@gmail.com>
2021-03-11 18:29:46 -0700

    added search bar in dashboard

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -8,8 +8,8 @@ import LoginWrapper from '../LoginWrapper';
 import Header from '../Header';
 import './dashboard.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faChevronDown, faFilter, faSearch} from '@fortawesome/free-solid-svg-icons';
-
+import {faChevronDown, faFilter, faSearch, faPlus} from '@fortawesome/free-solid-svg-icons';
+// i need to account for the margins and padding so flex basis wont freak out
 export class Dashboard extends React.Component{
     // componentDidMount(){   
     //     this.props.dispatch(fetchChampions())
@@ -44,6 +44,24 @@ export class Dashboard extends React.Component{
                             <div className='left-container-search-input'>
                                 <FontAwesomeIcon icon={faSearch} className=' search-icon'/>
                                 <input type='text' placeholder='Search' className='left-container-search-input-text' />
+                            </div>
+                            <div className='left-container-search-people'>
+                                <button className='add-button'><FontAwesomeIcon icon={faPlus} className='default-button-size'/></button>
+                                <div className='players'>
+                                    <div className='user-image-container search-img'>
+                                     <img alt='how the user sees themselves' className='user-image' src='https://avatars.githubusercontent.com/u/21373845?s=460&u=06623bc214c716a3ba9b90f0beea147d7b9cf6e1&v=4' />
+                                    </div>
+                                    <div className='user-image-container search-img'>
+                                     <img alt='how the user sees themselves' className='user-image' src='https://avatars.githubusercontent.com/u/21373845?s=460&u=06623bc214c716a3ba9b90f0beea147d7b9cf6e1&v=4' />
+                                    </div>
+                                    <div className='user-image-container search-img'>
+                                     <img alt='how the user sees themselves' className='user-image' src='https://avatars.githubusercontent.com/u/21373845?s=460&u=06623bc214c716a3ba9b90f0beea147d7b9cf6e1&v=4' />
+                                    </div>
+                                    <div className='user-image-container search-img'>
+                                     <img alt='how the user sees themselves' className='user-image' src='https://avatars.githubusercontent.com/u/21373845?s=460&u=06623bc214c716a3ba9b90f0beea147d7b9cf6e1&v=4' />
+                                    </div>
+                                </div>
+                                <p className='player-followers'><span className='not-important-color'>and</span> 24 followers</p>
                             </div>
                         </div>
                     </div>
