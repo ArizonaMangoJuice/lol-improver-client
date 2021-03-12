8383f274782dfd3019204ccd8d29e13ec1048336
Isael Lizama <isaelwebdeveloper@gmail.com>
2021-03-11 20:50:12 -0700

    disabled with router for now

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter as Router} from 'react-router-dom';
+// import {BrowserRouter as Router} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from './store';
 import App from './components/App';
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router>
+        {/* <Router> */}
             <App />
-        </Router>
+        {/* </Router> */}
     </Provider>
     , document.getElementById('root'));
 registerServiceWorker();
