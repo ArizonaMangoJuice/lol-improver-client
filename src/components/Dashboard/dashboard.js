bf91ac10afa13cf267f8047944c6a9d4d2f35ed9
Isael Lizama <isaelwebdeveloper@gmail.com>
2021-03-13 20:54:20 -0700

    added user-match-history-stub

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -11,6 +11,7 @@ import './dashboard.css';
 import DashBoardSearchBar from '../DashboardSearchBar';
 import DashboardCreateNote from '../DashboardCreateNote';
 import Masonry from 'react-responsive-masonry';
+import UserProfileImage from '../UserProfileImage';
 
 // I will use a masonry package but will look at source code to make my own.
 
@@ -38,11 +39,29 @@ export class Dashboard extends React.Component {
                                 <Note />
                                 <Note img={true} />
                                 <Note />
+                                <Note img={true} />
+                                <Note />
+                                <Note />
+                                <Note />
+                                <Note />
+                                <Note img={true} />
+                                <Note />
                             </Masonry>
                         </div>
                     </div>
                     <div className='dashboard-right-side'>
-
+                        <div className='dashboard-user-match-history'>
+                            <h2>Match History</h2>
+                            <div className='user-match-history'>
+                                <p className='user-match-time'>1 Day Ago</p>
+                                <div className='outcome'></div>
+                                <div className='user-match-history-right-side'>
+                                    <UserProfileImage src="http://opgg-static.akamaized.net/images/lol/champion/Ezreal.png?image=c_scale,q_auto,w_140&v=1614735388" />
+                                    <p className='user-champion-name'>Ezreal</p>
+                                    <p className='user-champion-kda'>13/4/5</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 {/* <ChampionInfo /> */}
