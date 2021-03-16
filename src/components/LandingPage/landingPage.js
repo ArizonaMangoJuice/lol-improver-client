35c370e72d18885d90efc6ecc0d5c61205b45d76
AMJ <isaelwebdeveloper@gmail.com>
2021-03-15 18:09:13 -0600

    added Sign in component without logic

--- src/components/LandingPage/landingPage.js
+++ src/components/LandingPage/landingPage.js
@@ -12,12 +12,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStickyNote, faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import SignUp from '../SignUp';
+import SignIn from '../SignIn';
 
 export const LandingPage = (props) => {
     // const redirect = (username, password) => {
     //     return props.dispatch(login(username, password))
     // }
+    // convert to use reducer 
     const [signUp, setSignUp] = useState(false);
+    const [signIn, setSignIn] = useState(false);
+
     // if(this.props.loggedIn){
     //     return <Redirect to='/dashboard'/>
     // }
@@ -25,10 +29,12 @@ export const LandingPage = (props) => {
     return (
         <main className='landing-page-container'>
             {signUp ? <SignUp setSignUp={setSignUp} signUp={signUp} /> : undefined}
+            {signIn ? <SignIn setSignIn={setSignIn} signIn={signIn} /> : undefined}
             <header className='landing-page-header'>
                 <h3 className='landing-page-title'>lol Improver Site Under Construction</h3>
                 <nav className='landing-page-nav'>
-                    <Link className='landing-links' to='/dashboard'>Features</Link>
+                    {/* <Link className='landing-links' to='/dashboard'>Features</Link> */}
+                    <button onClick={() => setSignIn(true)} className='landing-links'>Sign In</button>
                     <Link className='landing-links' to='/dashboard'>Search</Link>
                     <Link className='landing-links' to='/dashboard'>Dashboard</Link>
                     {/* <button className='landing-links'></button> */}
---
+++ src/components/SignIn/SignIn.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// 
+const SignIn = ({setSignIn, signIn}) => {
+    return (
+        <section className='sign-up-container'>
+            <section className='sign-up-bg'  onClick={() => setSignIn(false)}/>
+            <div className='sign-up '>
+                <h1 className='sign-up-h1'>Sign In</h1>
+                <form className='sign-up-form'>
+                        <input className='sign-up-input' type='email' placeholder='Username'/>
+                        <input className='sign-up-input' type='password' placeholder='Password'/>
+                        <button className='sign-up-button'>
+                            <p className='sign-up-button-p'>Sign Up</p>
+                        </button>
+                        
+                </form>
+            </div>
+        </section>
+    )
+}
+
+export default SignIn;
