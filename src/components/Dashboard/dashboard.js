5b9f8d8561c5d49370dbb3f0ad23d2cdfd19a554
Isael Lizama <isaelwebdeveloper@gmail.com>
2021-03-16 16:11:43 -0600

    added custom hook that detects width

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import {connect} from 'react-redux';
 // import fetchChampions from '../../actions/champions';
 // import LoginWrapper from '../LoginWrapper';
@@ -16,22 +16,61 @@ import UserMatchHistory from '../UserMatchHistory';
 // I will use a masonry package but will look at source code to make my own.
 
 // i need to account for the margins and padding so flex basis wont freak out
-export class Dashboard extends React.Component {
+export const Dashboard = (props) => {
     // componentDidMount(){   
     //     this.props.dispatch(fetchChampions())
     // }
+    const [windowWidth, setWidth] = useState(window.innerWidth);
 
-    // will add masonry package but will read source code to create my own
-    render() {
-        return (
-            <main className='main-content'>
-                <Header />
-                <div className='dashboard'>
-                    <div className='dashboard-left-side'>
-                        <DashboardCreateNote />
-                        <DashBoardSearchBar />
-                        <div className='notes-container left-container-width'>
-                            <Masonry columnsCount={4}>
+useEffect(() => {
+   
+    let timeoutId = null;
+    const resizeListener = () => {
+      // prevent execution of previous setTimeout
+      clearTimeout(timeoutId);
+      // change width from the state object after 150 milliseconds
+      timeoutId = setTimeout(() => setWidth(window.innerWidth), 150);
+    };
+
+    window.addEventListener("resize", resizeListener);
+
+    return () => {
+        window.addEventListener("resize", resizeListener);
+    }
+})
+
+console.log(windowWidth)
+
+// will add masonry package but will read source code to create my own
+return (
+    <main className='main-content'>
+        <Header />
+        <div className='dashboard'>
+            <div className='dashboard-left-side'>
+                <DashboardCreateNote />
+                <DashBoardSearchBar />
+                <div className='notes-container left-container-width'>
+                    {windowWidth < 600
+                        ? (
+                            <div>
+                                <Note img={true} />
+                                <Note />
+                                <Note />
+                                <Note />
+                                <Note />
+                                <Note img={true} />
+                                <Note />
+                                <Note img={true} />
+                                <Note />
+                                <Note />
+                                <Note />
+                                <Note />
+                                <Note img={true} />
+                                <Note />
+                            </div>
+                        )
+                        : (
+                            <Masonry columnsCount={4} className='test'>
                                 <Note img={true} />
                                 <Note />
                                 <Note />
@@ -47,25 +86,25 @@ export class Dashboard extends React.Component {
                                 <Note img={true} />
                                 <Note />
                             </Masonry>
-                        </div>
-                    </div>
-                    <div className='dashboard-right-side'>
-                        <h2 className='dashboard-match-history-title'>Match History</h2>
-                        <UserMatchHistory />
-                        <UserMatchHistory />
-                        <UserMatchHistory />
-                        <UserMatchHistory />
-                        <UserMatchHistory />
-                        <UserMatchHistory />
-
-                    </div>
+                        )}
                 </div>
-                {/* <ChampionInfo /> */}
-                {/* <NoteArea /> */}
-                {/* <PlayerSearch /> */}
-            </main>
-        )
-    }
+            </div>
+            <div className='dashboard-right-side'>
+                <h2 className='dashboard-match-history-title'>Match History</h2>
+                <UserMatchHistory />
+                <UserMatchHistory />
+                <UserMatchHistory />
+                <UserMatchHistory />
+                <UserMatchHistory />
+                <UserMatchHistory />
+
+            </div>
+        </div>
+        {/* <ChampionInfo /> */}
+        {/* <NoteArea /> */}
+        {/* <PlayerSearch /> */}
+    </main>
+)
 }
 
 // export default LoginWrapper()(connect()(Dashboard));
