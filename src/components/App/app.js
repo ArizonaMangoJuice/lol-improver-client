7bdd152c5b1aacf7ad6c31077c98730feaed6961
Isael Lizama <isaelwebdeveloper@gmail.com>
2021-03-17 10:31:14 -0600

    added to test function to check if the client and sever are linked

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,27 +1,38 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
 // import { withRouter} from 'react-router-dom';
-import {Route} from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import lolImproverUrl from '../../config';
 import Dashboard from '../Dashboard';
 import LandingPage from '../LandingPage';
 // import Header from '../Header';
 // import Footer from '../Footer';
 // import RegisterPage from '../RegisterPage';
 
-export class App extends React.Component{
-    render(){
-        
-        return (
-            <section className='main-body'>
-                {/* <Header /> */}
-                <Route exact path='/' component={LandingPage} />
-                <Route exact path='/dashboard' component={Dashboard} />
-                {/* <Dashboard /> */}
-                {/* <Route exact path='/register' component={RegisterPage}/> */}
-                {/* <Footer /> */}
-            </section>
-        );
-    }
+export const App = (props) => {
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        async function fetchTest() {
+            let response = await fetch(`${lolImproverUrl}/static/test`);
+            response = await response.json();
+            setData(response)
+        }
+
+        fetchTest();
+    }, [])
+
+    if (data) console.log(data);
+    return (
+        <section className='main-body'>
+            {/* <Header /> */}
+            <Route exact path='/' component={LandingPage} />
+            <Route exact path='/dashboard' component={Dashboard} />
+            {/* <Dashboard /> */}
+            {/* <Route exact path='/register' component={RegisterPage}/> */}
+            {/* <Footer /> */}
+        </section>
+    );
 }
 
 // export default withRouter(connect()(App));
