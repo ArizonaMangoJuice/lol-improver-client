138c10f5516f2f975b1706cde6144a2326992f83
Isael Lizama <isaelwebdeveloper@gmail.com>
2021-03-17 20:16:25 -0600

    added sign in to sign in componnet

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {connect} from 'react-redux';
+import { login } from '../../actions/auth';
+
 // 
 const SignIn = ({setSignIn, signIn, ...props}) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const buttonSignIn = (e, user) => {
+        e.preventDefault();
+        props.dispatch(login(user));
+    }
+
     return (
         <section className={`sign-up-container ${props.hidden ? 'hidden' : ''}`}>
             <section className='sign-up-bg'  onClick={() => setSignIn(false)}/>
             <div className='sign-up '>
                 <h1 className='sign-up-h1'>Sign In</h1>
                 <form className='sign-up-form'>
-                        <input className='sign-up-input' type='email' placeholder='Username'/>
-                        <input className='sign-up-input' type='password' placeholder='Password'/>
-                        <button className='sign-up-button'>
+                        <input
+                            onChange={e => setUsername(e.currentTarget.value)}
+                            className='sign-up-input' type='text' placeholder='Username'/>
+                        <input 
+                            onChange={e => setPassword(e.currentTarget.value)}
+                            className='sign-up-input' type='password' placeholder='Password'/>
+                        <button className='sign-up-button' onClick={(e) => buttonSignIn(e, {username, password})}>
                             <p className='sign-up-button-p'>Sign Up</p>
                         </button>
                 </form>
@@ -18,4 +33,4 @@ const SignIn = ({setSignIn, signIn, ...props}) => {
     )
 }
 
-export default SignIn;
+export default connect ()(SignIn);
