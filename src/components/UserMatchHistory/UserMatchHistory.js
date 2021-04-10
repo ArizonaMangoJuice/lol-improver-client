b96f575448230655a1a480961704b302ad45642c
AMJ <isaelwebdeveloper@gmail.com>
2021-04-09 20:08:34 -0600

    user can now search matches there still no error checking

--- src/components/UserMatchHistory/UserMatchHistory.js
+++ src/components/UserMatchHistory/UserMatchHistory.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import UserProfileImage from '../UserProfileImage';
 import './UserMatchHistory.css';
+import lolImproverUrl from '../../config';
+
 
 const UserMatchHistory = ({ match }) => {
+    const [champ, setChamp] = useState({});
     // move to own function
     // const time = parseInt(match.timestamp);
     const readableTime = new Date(parseInt(match.timestamp, 10));
@@ -12,17 +15,57 @@ const UserMatchHistory = ({ match }) => {
     const yyyy = readableTime.getFullYear();
 
     const gameDate = `${mm}/${dd}/${yyyy}`;
+
+    // move this to own hook file to grab matchImages
+    // useCallback for this
+    useEffect(() => {
+        const test = async () => {
+            let response = await fetch(`${lolImproverUrl}/api/static/${match.champion}`);
+            response = await response.json();
+            setChamp(response[0]);
+        }
+
+        test();
+
+    }, [match.champion])
+
+    // move to own useEffect file
+    const [player, setPlayer] = useState({});
+
+    useEffect(() => {
+        const player = match.participants.filter( player => player.championId.toString() === match.champion.toString())[0];
+        // console.log('player id', player.teamId, player)
+        const outcome = match.teams.filter(team => team.teamId === player.teamId)[0].win;
+        // console.log('this is the outcome', match.teams, outcome);
+        player.outcome = outcome;
+        setPlayer(player);
+    }, [match])
     
+
+    let kills = 0;
+    let deaths = 0;
+    let assists = 0;
+    let win;
+    
+    if(player && player.stats) kills = player.stats.kills;
+    if(player && player.stats) deaths = player.stats.deaths;
+    if(player && player.stats) assists = player.stats.assists;
+    if(player && player.outcome) player.outcome === 'Fail' ? win = false :  win = true;
+
+    // console.log('this is the champion info', player)
+
     return (
         <div className='dashboard-user-match-history'>
             <div className='user-match'>
                 <p className='user-match-time'>{gameDate}</p>
-                <div className='outcome'></div>
+                <div className={`outcome ${win ? 'win' : ''}`}></div>
                 <div className='user-match-history-right-side'>
-                    <UserProfileImage divStyles='user-match-history-img-container' imgStyles='user-match-history-img' src="http://opgg-static.akamaized.net/images/lol/champion/Ezreal.png?image=c_scale,q_auto,w_140&v=1614735388" />
+                    <UserProfileImage 
+                        divStyles='user-match-history-img-container' imgStyles='user-match-history-img' 
+                        src={`http://ddragon.leagueoflegends.com/cdn/11.7.1/img/champion/${champ.name}.png`} />
                     <div className='user-match-history-text'>
-                        <p className='user-champion-name'>Ezreal</p>
-                        <p className='user-champion-kda'>13/4/5</p>
+                        <p className='user-champion-name'>{champ.name}</p>
+                        <p className='user-champion-kda'>{kills}/{deaths}/{assists}</p>
                     </div>
                 </div>
             </div>
