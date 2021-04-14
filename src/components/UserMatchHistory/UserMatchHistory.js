f9583df944d0d69c243141f4b95a4e6fcdd19bbe
AMJ <isaelwebdeveloper@gmail.com>
2021-04-13 20:38:42 -0600

    created an async function that runs in useffect that will grab the images for the rest of the players and shows stats

--- src/components/UserMatchHistory/UserMatchHistory.js
+++ src/components/UserMatchHistory/UserMatchHistory.js
@@ -5,13 +5,14 @@ import useGetPlayer from "../../hooks/useGetPlayer";
 import useGetMatchImage from "../../hooks/useGetMatchImage";
 import createMatchDate from "../../utils/createMatchDate";
 import { useGetTeam } from "../../hooks/useGetTeam";
+import lolImproverUrl from '../../config';
 
 const UserMatchHistory = ({ match }) => {
   const champ = useGetMatchImage(match);
   const player = useGetPlayer(match);
   const gameDate = createMatchDate(match);
   const teamOne = useGetTeam(match, 100);
-  const teamTwo = useGetTeam(match, 200);
+  // const teamTwo = useGetTeam(match, 200);
   const [firstTeam, setFirstTeam] = useState([]);
   let kills = 0;
   let deaths = 0;
@@ -25,27 +26,64 @@ const UserMatchHistory = ({ match }) => {
   if (player && player.outcome)
     player.outcome === "Fail" ? (win = false) : (win = true);
 
+  //move to own useEffect hook file
   useEffect(() => {
     if (teamOne.length !== 0) {
-      console.log(teamOne);
-      const firstTeamInfo = teamOne.map(({ pIdentity, ...player }) => (
-        <section>
-          <div className="user-match-history-right-side">
-            {/* <UserProfileImage
-                              divStyles='user-match-history-img-container' imgStyles='user-match-history-img'
-                              src={`http://ddragon.leagueoflegends.com/cdn/11.7.1/img/champion/${champ && champ.name ? champ.name.replace(/\s+/g, '') : ''}.png`} /> */}
-            <div className="user-match-history-text">
-              <p className="user-champion-name">
-                {pIdentity[0].player.summonerName}
-              </p>
-              {/* <p className='user-champion-kda'>{kills}/{deaths}/{assists}</p> */}
-            </div>
-          </div>
-        </section>
-      ));
-      setFirstTeam(() => firstTeamInfo);
+      let test = async () => {
+        const players = await otherPlayersCard();
+        setFirstTeam(() => players);
+      }
+      test();
     }
   }, [teamOne]);
+
+  console.log('THIS IS THE FIRST TEAM', firstTeam)
+
+
+  // move to own function and refactor a bit 
+  async function otherPlayersCard() {
+    const firstTeamInfo = await teamOne.map(async (player) => {
+      let champion = await fetch(`${lolImproverUrl}/api/static/${player.championId}`);
+      champion = await champion.json();
+      return { ...player, champion };
+    });
+
+    let result = await Promise.all(firstTeamInfo);
+    console.log('THIS IS THE RESULTING ARRAY', result)
+    result = result.map(({ champion, pIdentity, ...player }) =>
+    (
+      <section key={`${champion} + ${pIdentity[0].player.summonerName}`}>
+        <div className="user-match-history-right-side">
+          <UserProfileImage
+            divStyles='user-match-history-img-container' imgStyles='user-match-history-img'
+            src={`http://ddragon.leagueoflegends.com/cdn/11.7.1/img/champion/${champion && champion[0] && champion[0].name ? champion[0].image.full.replace(/\s+/g, '') : ''}`} />
+          <div className="user-match-history-text">
+            <p className="user-champion-name">
+              {pIdentity[0].player.summonerName}
+            </p>
+            <p className='user-champion-kda'>{player.stats.kills}/{player.stats.deaths}/{player.stats.assists}</p>
+          </div>
+        </div>
+      </section>
+    ))
+
+    return result;
+  }
+  // (
+  //   <section>
+  //     <div className="user-match-history-right-side">
+  //       {/* <UserProfileImage
+  //                         divStyles='user-match-history-img-container' imgStyles='user-match-history-img'
+  //                         src={`http://ddragon.leagueoflegends.com/cdn/11.7.1/img/champion/${champ && champ.name ? champ.name.replace(/\s+/g, '') : ''}.png`} /> */}
+  //       <div className="user-match-history-text">
+  //         <p className="user-champion-name">
+  //           {pIdentity[0].player.summonerName}
+  //         </p>
+  //         {/* <p className='user-champion-kda'>{kills}/{deaths}/{assists}</p> */}
+  //       </div>
+  //     </div>
+  //   </section>
+  // )
 
   //   useEffect(() => {
   //     let mounted = true;
@@ -88,9 +126,8 @@ const UserMatchHistory = ({ match }) => {
           <UserProfileImage
             divStyles="user-match-history-img-container"
             imgStyles="user-match-history-img"
-            src={`http://ddragon.leagueoflegends.com/cdn/11.7.1/img/champion/${
-              champ && champ.name ? champ.name.replace(/\s+/g, "") : ""
-            }.png`}
+            src={`http://ddragon.leagueoflegends.com/cdn/11.7.1/img/champion/${champ && champ.name ? champ.name.replace(/\s+/g, "") : ""
+              }.png`}
           />
           <div className="user-match-history-text">
             <p className="user-champion-name">{champ.name}</p>
