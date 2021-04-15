d2b605eecf014aec1f5cc652caf2af9c20d8dbfa
AMJ <isaelwebdeveloper@gmail.com>
2021-04-15 09:03:26 -0600

    added conditional to check if item was empty need to refactor

--- src/components/PlayerMatchCard/PlayerMatchCard.js
+++ src/components/PlayerMatchCard/PlayerMatchCard.js
@@ -5,6 +5,7 @@ import './PlayerMatchCard.css';
 
 export const PlayerMatchCard = ({ matchDetails }) => {
     const { champion, pIdentity, ...player } = matchDetails;
+    console.log('this is the match details', matchDetails)
     return (
         <section className='player-match-card-container' key={`${champion} + ${pIdentity[0].player.summonerName}`}>
             <div className="player-match-card">
@@ -20,14 +21,43 @@ export const PlayerMatchCard = ({ matchDetails }) => {
                     <p className='user-champion-kda'>{player.stats.kills}/{player.stats.deaths}/{player.stats.assists}</p>
                 </section>
                 <UserSummonerSpells player={player} />
+                {/* refactor this so you dont have to do one by one */}
                 <section className='user-items'>
-                    <img alt={`item of ${player.stats.item0}`} className='item' src={`http://ddragon.leagueoflegends.com/cdn/11.8.1/img/item/${player.stats.item0}.png`} />
-                    <img alt={`item of ${player.stats.item1}`} className='item' src={`http://ddragon.leagueoflegends.com/cdn/11.8.1/img/item/${player.stats.item1}.png`} />
-                    <img alt={`item of ${player.stats.item2}`} className='item' src={`http://ddragon.leagueoflegends.com/cdn/11.8.1/img/item/${player.stats.item2}.png`} />
-                    <img alt={`item of ${player.stats.item3}`} className='item' src={`http://ddragon.leagueoflegends.com/cdn/11.8.1/img/item/${player.stats.item3}.png`} />
-                    <img alt={`item of ${player.stats.item4}`} className='item' src={`http://ddragon.leagueoflegends.com/cdn/11.8.1/img/item/${player.stats.item4}.png`} />
-                    <img alt={`item of ${player.stats.item5}`} className='item' src={`http://ddragon.leagueoflegends.com/cdn/11.8.1/img/item/${player.stats.item6}.png`} />
-                    <img alt={`item of ${player.stats.item6}`} className='item' src={`http://ddragon.leagueoflegends.com/cdn/11.8.1/img/item/${player.stats.item6}.png`} />
+                    {
+                        player.stats.item0 !== 0
+                            ? <img alt={`item of ${player.stats.item0}`} className='item' src={`http://ddragon.leagueoflegends.com/cdn/11.8.1/img/item/${player.stats.item0}.png`} />
+                            : null
+                    }
+                    {
+                        player.stats.item1 !== 0
+                            ? <img alt={`item of ${player.stats.item1}`} className='item' src={`http://ddragon.leagueoflegends.com/cdn/11.8.1/img/item/${player.stats.item1}.png`} />
+                            : null
+                    }
+                    {
+                        player.stats.item2 !== 0
+                            ? <img alt={`item of ${player.stats.item2}`} className='item' src={`http://ddragon.leagueoflegends.com/cdn/11.8.1/img/item/${player.stats.item2}.png`} />
+                            : null
+                    }
+                    {
+                        player.stats.item3 !== 0
+                            ? <img alt={`item of ${player.stats.item3}`} className='item' src={`http://ddragon.leagueoflegends.com/cdn/11.8.1/img/item/${player.stats.item3}.png`} />
+                            : null
+                    }
+                    {
+                        player.stats.item4 !== 0
+                            ? <img alt={`item of ${player.stats.item4}`} className='item' src={`http://ddragon.leagueoflegends.com/cdn/11.8.1/img/item/${player.stats.item4}.png`} />
+                            : null
+                    }
+                    {
+                        player.stats.item5 !== 0
+                            ? <img alt={`item of ${player.stats.item5}`} className='item' src={`http://ddragon.leagueoflegends.com/cdn/11.8.1/img/item/${player.stats.item5}.png`} />
+                            : null
+                    }
+                    {
+                        player.stats.item6 !== 0
+                            ? <img alt={`item of ${player.stats.item6}`} className='item' src={`http://ddragon.leagueoflegends.com/cdn/11.8.1/img/item/${player.stats.item6}.png`} />
+                            : null
+                    }
                 </section>
             </div>
         </section>
