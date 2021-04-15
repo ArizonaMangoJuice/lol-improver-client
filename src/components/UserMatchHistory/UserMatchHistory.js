82bdc785f46955ed6884f1a738d836c0fda1a073
AMJ <isaelwebdeveloper@gmail.com>
2021-04-15 11:32:06 -0600

    short circuited main player stats and moved otherPlayerCard to own file

--- src/components/UserMatchHistory/UserMatchHistory.js
+++ src/components/UserMatchHistory/UserMatchHistory.js
@@ -5,8 +5,7 @@ import useGetPlayer from "../../hooks/useGetPlayer";
 import useGetMatchImage from "../../hooks/useGetMatchImage";
 import createMatchDate from "../../utils/createMatchDate";
 import { useGetTeam } from "../../hooks/useGetTeam";
-import lolImproverUrl from '../../config';
-import PlayerMatchCard from "../PlayerMatchCard";
+import otherPlayersCard from '../../utils/otherPlayersCard';
 
 const UserMatchHistory = ({ match }) => {
   const champ = useGetMatchImage(match);
@@ -16,18 +15,6 @@ const UserMatchHistory = ({ match }) => {
   const teamTwo = useGetTeam(match, 200);
   const [firstTeam, setFirstTeam] = useState([]);
   const [secondTeam, setSecondTeam] = useState([]);
-
-  let kills = 0;
-  let deaths = 0;
-  let assists = 0;
-  let win;
-  //   let firstTeam = [];
-
-  if (player && player.stats) kills = player.stats.kills;
-  if (player && player.stats) deaths = player.stats.deaths;
-  if (player && player.stats) assists = player.stats.assists;
-  if (player && player.outcome)
-    player.outcome === "Fail" ? (win = false) : (win = true);
 
   //move to own useEffect hook file
   useEffect(() => {
@@ -50,36 +37,11 @@ const UserMatchHistory = ({ match }) => {
     }
   }, [teamTwo]);
 
-  // console.log('THIS IS THE FIRST TEAM', firstTeam)
-
-
-  // move to own function and refactor a bit 
-  async function otherPlayersCard(team) {
-    const firstTeamInfo = await team.map(async (player) => {
-      let champion = await fetch(`${lolImproverUrl}/api/static/${player.championId}`);
-      champion = await champion.json();
-      return { ...player, champion };
-    });
-
-    let result = await Promise.all(firstTeamInfo);
-    // console.log('THIS IS THE RESULTING ARRAY', result)
-    result = result.map((matchDetails) => (
-      <PlayerMatchCard
-        key={`${matchDetails.pIdentity[0].player.summonerName} 
-              ${matchDetails.champion[0].id} ${matchDetails.pIdentity[0].participantId}
-              ${matchDetails.pIdentity[0].accountId}
-              `}
-        matchDetails={matchDetails} />
-    ))
-    return result;
-  }
-
-
   return (
     <div className="dashboard-user-match-history">
       <div tabIndex="1" className="user-match">
         <p className="user-match-time">{gameDate}</p>
-        <div className={`outcome ${win ? "win" : ""}`}></div>
+        <div className={`outcome ${player && player.outcome !== "Fail" ? "win" : ""}`}></div>
         <div className="user-match-history-right-side">
           <UserProfileImage
             divStyles="user-match-history-img-container"
@@ -90,7 +52,7 @@ const UserMatchHistory = ({ match }) => {
           <div className="user-match-history-text">
             <p className="user-champion-name">{champ.name}</p>
             <p className="user-champion-kda">
-              {kills}/{deaths}/{assists}
+              {player && player.stats && player.stats.kills}/{player && player.stats && player.stats.deaths}/{player && player.stats && player.stats.assists}
             </p>
           </div>
         </div>
